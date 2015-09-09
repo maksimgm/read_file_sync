43c0473effa2c133a5b7d5b84e61095de719542b
maksimgm <maksimmamrikov@gmail.com>
2015-09-09 14:41:26 -0700

    add script use read file

--- require.js
+++ require.js
@@ -1,11 +1,23 @@
+// Using read file sync
+
+// var fs = require('fs');
+
+// var content = fs.readFileSync(process.argv[2]);
+
+// var string = content.toString();
+
+// var lineCounter = string.split('\n');
+
+// console.log(lineCounter.length-1);
+
+
+// Using read file
+
 var fs = require('fs');
 
-var content = fs.readFileSync(process.argv[2]);
+var content = fs.readFile(process.argv[2], "utf8",function(error,data){
 
-var string = content.toString();
+console.log(data.split('\n').length-1);
 
-var lineCounter = string.split('\n');
+});
 
-console.log(lineCounter.length-1);
-
-// length
